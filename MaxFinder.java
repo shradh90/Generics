76bd5c9a117266c09af57770bd184353b6c4b61e
shradh90 <shradhad1996@gmail.com>
2021-06-22 19:31:36 +0530

    refractor-2

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,32 +1,51 @@
 package com.shradha.max;
-import javax.swing.text.DefaultEditorKit;
-import java.util.Comparator;
-import java.util.Scanner;
 
-public class MaxFinder {
-    //creating generic method and  this method extends comparable and returns max value
-    public static <T extends Comparable<T>> T findMax (T[] inputArray){
+
+public class MaxFinder<T extends Comparable<T>> {
+
+    //declaring instance variables
+    T [] arrayOfInt,arrayOfFloat,arrayOfString;
+
+    public MaxFinder(T[] arrayOfInt, T[] arrayOfFloat, T[] arrayOfString) {
+        this.arrayOfInt = arrayOfInt;
+        this.arrayOfFloat = arrayOfFloat;
+        this.arrayOfString = arrayOfString;
+    }
+    //parameterized constructor
+
+
+    public static void main(String[] args) {
+
+        Integer[] a = {22,43,22};
+        Float [] b = {22.5f,34.43f,34.33f};
+        String[] c = {"apple","cat","banana"};
+
+        new MaxFinder(a,b,c).testMaximum();
+
+
+    }
+
+    private void testMaximum() {
+
+        //passing instance variable to static testMaximum method
+        Integer maxOfThreeInt = (Integer) testMaximum(arrayOfInt);
+        System.out.println("The maximum value of three integers is : "+maxOfThreeInt);
+
+        Float maxOfThreeFloat = (Float) testMaximum(arrayOfFloat);
+        System.out.println("The maximum value of three floats is : "+maxOfThreeFloat);
+
+        String maxOfThreeString = (String) testMaximum(arrayOfString);
+        System.out.println("The maximum value of three string is : "+maxOfThreeString);
+    }
+
+    //displays maximum of three values
+    static <T extends Comparable<T>> T testMaximum(T[] inputArray) {
         T max = inputArray[0];
         for(T t : inputArray){
             if(t.compareTo(max)>0)
-                max = t;
+                max=t;
         }
         return max;
     }
-    public static void main(String[] args) {
-
-        Integer[] arrayOfInteger = {33,22,43};
-        Float[] arrayOfFloats = {22.34f,34.43f,34.44f};
-        String[] arrayOfStrings = {"apple","banana","cat"};
-
-        Integer maxOfThreeInteger = findMax(arrayOfInteger);
-        System.out.println("The maximum value of three integers is : "+maxOfThreeInteger);
-
-        Float  maxOfThreeFloats = findMax(arrayOfFloats);
-        System.out.println("The maximum value of three floats is : "+maxOfThreeFloats);
-
-        String maxOfThreeStrings = findMax(arrayOfStrings);
-        System.out.println("The maximum value of three strings is : "+maxOfThreeStrings);
-
-    }
 }
+© 2021 GitHub, Inc.
